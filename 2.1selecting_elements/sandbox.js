ff8d40c25d929788150531364dfdb393481d89c4
kulraj <kulraj@vinsol.com>
2013-06-29 16:51:58 +0530

    selecting elements done

--- 2.1selecting_elements/sandbox.js
+++ 2.1selecting_elements/sandbox.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    alert("ready");
-}
+    var count_hidden, count_alt;
+    $("div.module");
+
+    $("#myListItem").addClass("green");
+    $("#myList li:nth-child(3)");
+    $("li[id='myListItem']");
+    // i think second method is best since we may change the elements or id 
+    //but the query will always give the third child.
+
+    $("label[for='q']").addClass("blue");
+
+    count_hidden = $(":hidden").length;
+    alert("hidden elements= " + count_hidden);
+
+    count_alt = $("[alt]").length;
+    alert("elements with alt= " + count_alt);
+
+    $("tr:odd").addClass("red");
+    });
