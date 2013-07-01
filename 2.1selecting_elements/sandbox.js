813e136cc8f263ab0b1376d009668d8e39b175eb
kulraj <kulraj@vinsol.com>
2013-07-01 18:11:34 +0530

    console.log for message, different method for selection, comments added

--- 2.1selecting_elements/sandbox.js
+++ 2.1selecting_elements/sandbox.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
     var count_hidden, count_alt;
+    //1
     $("div.module").addClass("yellow");
 
+    //2
     $("#myListItem").addClass("green");
     $("#myList li:nth-child(3)");
-    $("li[id='myListItem']");
-    // i think second method is best since we may change the elements or id 
-    //but the query will always give the third child.
+    $("#myList li").eq(2);
+    // i think first method is best since we are directly matching by the id
 
+    //3
     $("label[for='q']").addClass("blue");
 
+    //4
     count_hidden = $(":hidden").length;
-    alert("hidden elements= " + count_hidden);
+    console.log("hidden elements= " + count_hidden);
 
+    //5
     count_alt = $("[alt]").length;
-    alert("elements with alt= " + count_alt);
+    console.log("elements with alt= " + count_alt);
 
+    //6
     $("tr:odd").addClass("red");
     });
