8685f01bc85e0ece7878d68c4df445f669721bef
kulraj <kulraj@vinsol.com>
2013-07-04 13:08:44 +0530

    reveal hidden text exercise done

---
+++ 5.1reveal_hidden_text/navigation.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#blog h3").bind("click", function (event) {
+    $this = $(this);
+    //prevent link from opening
+    event.preventDefault();
+    //2 slide up any other currently showing excerpt paragraphs
+    $("#blog p:visible").slideUp(1000);
+    //1 slide down the excerpt paragraph related to the clicked link
+    $this.next().slideDown(1000);
+  });
+});
