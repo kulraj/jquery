ebd707f0805e32b4f54f21a39b59392f945a2f5a
kulraj <kulraj@vinsol.com>
2013-07-04 17:01:06 +0530

    create drop down menu done

---
+++ 5.2create_dropdown_menu/navigation.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+$("#nav > li").hover(function () {
+     //show the submenu items
+     $(this).children("ul").addClass("hover");
+  },
+  function () {
+     //hide the submenu items
+     $(this).children("ul").removeClass("hover");
+  });
+});
