0c7ab6f68d41b35ac1790e8ac0bd673d967e74fc
kulraj <kulraj@vinsol.com>
2013-07-08 16:37:00 +0530

    delegate event exercise done

---
+++ live_stack/live_stack.js
@@ -0,0 +1,19 @@
+var $div_number = 1;
+
+// add a div to the stack with an incremental number(starting from 1)
+function addItem() {
+  $("<div></div>").text($div_number).appendTo("#container");
+  $div_number += 1;
+}
+
+$(function() {
+  $("#container").delegate("div","click", function () {
+    // highlight the clicked div
+    $(this).addClass("green");
+    $(this).siblings().removeClass("green");
+    //remove the last div if clicked
+    if ($(this).is(":last-child")) {
+      $(this).remove();
+    }
+  });
+});
