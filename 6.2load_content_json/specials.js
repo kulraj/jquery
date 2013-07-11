a2d8f7346d27edec21d249d17226d6d2b23af7ff
kulraj <kulraj@vinsol.com>
2013-07-11 19:12:58 +0530

    ajax called only once

--- 6.2load_content_json/specials.js
+++ 6.2load_content_json/specials.js
@@ -1,32 +1,40 @@
+function addHtmlToDiv($day_list, $div) {
+  var $previous_color;
+  // clear div and remove previous color classes
+  $div.empty();
+  $div.removeClass($previous_color);
+  //3 use the value the user selected in the select to look up information about the special in the JSON response.
+  var $day = $day_list[$("select option:selected").val().toLowerCase()];
+  //4 Add some HTML about the special to the target div
+  $("<h2></h2>").text($day.title).appendTo($div);
+  $("<p></p>").text($day.text).appendTo($div);
+  $("<img></img>").attr("src", $day.image.substring(1)).appendTo($div);
+  $div.addClass($day.color);
+  $previous_color = $day.color;
+  //5 remove the submit button
+  $("#specials li.buttons").remove();
+}
+
 $(document).ready(function() {
   //1 Append a target div after the form that's inside the #specials element
   var $div = $("<div></div>").insertAfter("#specials form");
+  var changed = false, $jsonText, $day_list;
 
   //2  Bind to the change event of the select element
   $("select").bind("change", function () {
-    $.ajax ( {// send an Ajax request to specials.json on selection change
-      url : "specials.json",
-      type : "GET",
-      dataType : "json",
-      success : function (specials) {
-        var $previous_color;
-        // clear div and remove previous color classes
-        $div.empty();
-        $div.removeClass($previous_color);
-        //3 use the value the user selected in the select to look up information about the special in the JSON response.
-        var $day = specials[$("select option:selected").val()];
-        //4 Add some HTML about the special to the target div
-        $("<h2></h2>").text($day.title).appendTo($div);
-        $("<p></p>").text($day.text).appendTo($div);
-        $("<img></img>").attr("src", $day.image.substring(1)).appendTo($div);
-        $div.addClass($day.color);
-        $previous_color = $day.color;
-        //5 remove the submit button
-        $("#specials li.buttons").remove();
-      }
-    });
+    if (!changed) {
+      $.ajax ( {// send an Ajax request to specials.json on selection change
+        url : "specials.json",
+        type : "GET",
+        dataType : "json",
+        success : function (specials) {
+          $jsonText = JSON.stringify(specials);
+          $day_list = JSON.parse($jsonText);
+          addHtmlToDiv($day_list, $div);
+        }
+      });
+    changed = true;
+    }
+  addHtmlToDiv($day_list, $div);
   });
-  // if we want to make the request only once, we can make the ajax call outside the event handler in main
-  // and only do the manipulations on div(which are inside the success function) inside the event handler
-  // and we can remove the submit button inside the ajax call since it needs to be done only once. 
 });
