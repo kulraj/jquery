5d2f70efd940cbc5a81e053db62d33c21a426dad
kulraj <kulraj@vinsol.com>
2013-07-11 11:21:53 +0530

    working in chrome fixed

--- accordian_implementation/state_retain.js
+++ accordian_implementation/state_retain.js
@@ -3,37 +3,18 @@ function getClickedItem() {
   return $url.split("?")[1];
 }
 
-function showDropdownChain($node) {
-  var $parent;
-  // chain from node up to parent
-  for (; $node.length; $node = $parent) {
-    $parent = $node.parent().parent().prev("a");
-    $node.addClass("show-dropdown");
-    //$node.slideDown();
-  }
-}
-
 $(function () {
   var $clicked_item = getClickedItem();
-
   var $links = $("a");
+
   $.each($links, function () {
     //add information of clicked item to url
     $(this).attr("href",$(this).attr("href") + "?" + $(this).text());
     // highlight clicked item and display its parent lists
     if ($clicked_item == $(this).text()) {
       $(this).addClass("clicked");
-      showDropdownChain($(this).parent().parent().prev());
-    }
-  });
-
-  // function to hide all lists other than the current one
-  $links.bind("click", function () {
-    $.each($links, function () {
-      $(this).removeClass("show-dropdown");
-    });
-    if ($(this).next().attr("class") == "accordion") {
-      showDropdownChain($(this));
+      // add active class to anchor tag pertaining to parent ul of clicked node
+      $(this).parent().parent().prev().addClass("active");
     }
   });
 });
