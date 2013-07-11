d2b68814f559d28126fcd053be2ff72649785644
kulraj <kulraj@vinsol.com>
2013-07-11 17:55:15 +0530

    functioning rectified, photos removed

--- 4.2tabbed_navigation/sandbox.js
+++ 4.2tabbed_navigation/sandbox.js
@@ -4,23 +4,29 @@ $(document).ready(function() {
   $(".module").addClass("hidden");
 
   //2 Create an unordered list element before the first module
-  $("<li> an unordered list element </li>").insertBefore(".module:first");
+  $("<ul id='list'></ul>").insertBefore(".module:first");
 
   //3 Iterate over the modules using $.fn.each.
   //For each module, use the text of the h2 element as the text for a list item
   //that you add to the unordered list element.
   $(".module").each(function() {
     $this = $(this);
-    $("<li>" + $this.children("h2").text() + "</li>").insertBefore(this);
+    $("<li>" + $this.children("h2").text() + "</li>").appendTo("#list");
   });
 
   //4 Bind a click event to the list item
-  $(".module").prev().bind('click', function() {
-    $this = $(this);
-    $this.next().removeClass("hidden");// Show the related module
-    $(".module").not($this.next()).addClass("hidden");//hide any other modules
-    $this.addClass("current"); //Add a class of "current" to the clicked list item
-    $(".module").prev().not($this).removeClass("current");//Remove the class "current" from the other list item
+  $("#list li").bind('click', function() {
+    var $clicked_list_item = $(this);
+    $(this).addClass("current");
+    $(this).siblings().removeClass("current");
+    // show the corresponding module and hide others
+    $(".module").each(function() {
+      if ($(this).attr("id") == $clicked_list_item.html().toLowerCase()) {
+        $(this).removeClass("hidden");
+      } else {
+        $(this).addClass("hidden");
+      }
+    });
   });
 
   //5 show the first tab
