17bf7d1285b733124ae239ce5c77537d3123760d
kulraj <kulraj@vinsol.com>
2013-07-11 12:59:45 +0530

    indentation rectified, unnecessary files removed

--- 5.2create_dropdown_menu/navigation.js
+++ 5.2create_dropdown_menu/navigation.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-$("#nav > li").hover(function () {
-     //show the submenu items
-     $(this).children("ul").addClass("hover");
+  $("#nav > li").hover(function () {
+    //show the submenu items
+    $(this).children("ul").addClass("hover");
   },
   function () {
-     //hide the submenu items
-     $(this).children("ul").removeClass("hover");
+    //hide the submenu items
+    $(this).children("ul").removeClass("hover");
   });
 });
