88d525133823b26a929a38f61dc8e4a8a5925f74
kulraj <kulraj@vinsol.com>
2013-07-12 11:01:14 +0530

    slideshow smoothened

--- 5.3slideshow/slideshow.js
+++ 5.3slideshow/slideshow.js
@@ -1,10 +1,4 @@
-function timerForSlideshow (i, $slideshow_list_items, time_for_one_item, fade_time) {
-  $slideshow_list_items.eq(i).fadeIn(fade_time).delay(time_for_one_item - fade_time * 2).fadeOut(fade_time);
-  // find which image it is
-  $("#which_image").find("li").text($slideshow_list_items.eq(i).children("img").attr("alt"));
-  i += 1;
-  return (i === $slideshow_list_items.length) ? 0 : i; //3 reloop when items end
-}
+var $slideshow_list_items;
 
 function createNavigationArea($slideshow) {
   var $navigation_area = $("<ul id='navigation_area'></ul>").insertAfter($slideshow);
@@ -13,22 +7,28 @@ function createNavigationArea($slideshow) {
   $("<ul><li></li></ul>").appendTo($navigation_area.children("li"));
 }
 
+function slideshow(i) {
+  $slideshow_list_items.eq(i).fadeIn( function () {
+    $("#which_image").find("li").text($slideshow_list_items.eq(i).children("img").attr("alt"));
+    $(this).delay(2000);
+    $(this).fadeOut( function () {
+      i = (i == 2) ? 0 : i + 1;
+      slideshow(i);
+    });
+  });
+}
+
 $(document).ready(function() {
   //1  Move the #slideshow element to the top of the body.
-  var $first_element = $("body").children().eq(0);
+  var $first_element = $("body").children(":first");
   var $slideshow = $("#slideshow");
   $slideshow.insertBefore($first_element);
 
   $("#slideshow li").hide(); 
-  var $slideshow_list_items = $("#slideshow li"), i = 0;
-
-  // you can easily change the timings for slideshow by changing these variables:
-  //but please keep fade time less than (time for one item) /2 ;-)
-  var time_for_one_item = 2000, fade_time = 200;
+  $slideshow_list_items = $("#slideshow li");
   createNavigationArea($slideshow);
 
-  //2 cycle through the list items inside the element
-  setInterval(function() {i = timerForSlideshow(i, $slideshow_list_items, time_for_one_item, fade_time); }, time_for_one_item);
+  slideshow(0);//start from first element
 
   // write the number of items
   $("#number_images").find("li").append($slideshow_list_items.length);
