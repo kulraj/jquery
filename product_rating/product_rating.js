2a6bce73701676a7862b2e50b1de60d73de8311c
kulraj <kulraj@vinsol.com>
2013-07-13 12:41:40 +0530

    modified as per given question

--- product_rating/product_rating.js
+++ product_rating/product_rating.js
@@ -6,11 +6,15 @@ $(function () {
     $(this).addClass("blue");
   });
 
-  $("input").bind("click", function () {
-    if ($("td[id='" + $(this).attr("name") + "']").attr("class").indexOf("blue") != -1) {
-      $("td").removeClass("blue");
-      $("td[id='" + $(this).attr("value") + "']").addClass("blue");
-      $("td[id='" + $(this).attr("name") + "']").addClass("blue");
-    }
+  $("td.rating").bind("click", function () {
+    var $selected_rating = $(this).attr("id");
+    $("td.item").each(function () {
+      if($(this).attr("class").indexOf("blue") != -1) {
+        var $item = $(this).attr("id");
+        $("td.rating").removeClass("blue");
+        $("input[name='" + $item + "'][value='" + $selected_rating + "']").attr("checked", true);
+        $("#" + $selected_rating).addClass("blue");
+      }
+    });
   });
 });
