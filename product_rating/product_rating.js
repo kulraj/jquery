b96f33852c12cc9e1a2d60d9d02b8d8698fa7b17
kulraj <kulraj@vinsol.com>
2013-07-19 13:12:31 +0530

    default prevented for radio buttons

--- product_rating/product_rating.js
+++ product_rating/product_rating.js
@@ -2,19 +2,21 @@ $(function () {
   $("input").attr("checked", false);
 
   $("td.item").bind("click", function() {
-    $("td").removeClass("blue");
+    $("td.item, td.rating").removeClass("blue");
     $(this).addClass("blue");
   });
 
   $("td.rating").bind("click", function () {
-    var $selected_rating = $(this).attr("id");
-    $("td.item").each(function () {
-      if($(this).attr("class").indexOf("blue") != -1) {
-        var $item = $(this).attr("id");
-        $("td.rating").removeClass("blue");
-        $("input[name='" + $item + "'][value='" + $selected_rating + "']").attr("checked", true);
-        $("#" + $selected_rating).addClass("blue");
-      }
+    var $selected_rating = $(this);
+    $("td.item.blue").each(function () {
+      var $item = $(this).attr("id");
+      $("td.rating").removeClass("blue");
+      $("input[name='" + $item + "'][value='" + $selected_rating.attr("id") + "']").attr("checked", true);
+      $selected_rating.addClass("blue");
     });
+  });
+
+  $("input").bind("click", function (event) {
+    event.preventDefault();
   });
 });
